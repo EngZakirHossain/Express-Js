4c5ee5b878fce20032f98fafd4c35f79987ff49c
Zakir Trimatric <zakirtrimatric@gmail.com>
2022-10-20 16:49:26 +0600

    server site request and test http request

--- server/app.js
+++ server/app.js
@@ -1,4 +1,17 @@
 const express = require("express");
 const app = express();
+app.get('/',(req, res)=>{
+    res.send("I am a get request ar home Route");
+});
+app.post('/',(req, res)=>{
+    res.send("I am a post request ar home Route");
+});
+app.put('/',(req, res)=>{
+    res.send("I am a put request ar home Route");
+});
+app.delete('/',(req, res)=>{
+    res.send("I am a delete request ar home Route");
+    res.end();
+});
 
 module.exports = app;
