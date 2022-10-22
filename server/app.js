22ef40790898c3786a108a715b505e6e701c658f
Zakir Trimatric <zakirtrimatric@gmail.com>
2022-10-22 14:10:11 +0600

    html file, json data

--- server/app.js
+++ server/app.js
@@ -4,8 +4,33 @@ const userRouter = require('./routes/users.route');
 const adminRouter = require('./routes/admin.route');
 
 //Home Route
-app.get('/',(req, res)=>{
-    res.send("I am a Get request at home Route");
+app.use('/',(req, res)=>{
+    // res.cookie("name","zakir hossain");
+    res.clearCookie("name");
+    res.append("id","13000");
+    res.end();
+    // res.send("I am a Get request at home Route");
+});
+//json Data send and
+app.use('/register',(req, res)=>{
+    // res.status(200).json({
+    //     name : 'Zakir Hossain',
+    //     message: 'This is register Page',
+    //     statusCode : 200,
+    // });
+    res.statusCode(200);
+    res.sendFile(__dirname+"/views/index.html");
+
+    // res.redirect('/login');
+});
+//Redirect url to login
+app.use('/login',(req, res)=>{
+    // res.cookie("name","zakir hossain");
+    // res.clearCookie("name");
+    // res.append("id","13000");
+    // res.end();
+    res.statusCode(200);
+    res.sendFile(__dirname+"/views/index.html");
 });
 //user route
 app.use("/api/user",userRouter);
