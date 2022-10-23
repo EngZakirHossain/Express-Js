8dfff3a77d6cbc80b120a482c0e5a53ce36b1b30
Zakir Trimatric <zakirtrimatric@gmail.com>
2022-10-23 12:52:59 +0600

    header request

--- requestServer/headerValue.js
+++ requestServer/headerValue.js
@@ -3,9 +3,9 @@ const  app = express();
 const PORT = 3000;
 const hostName = '127.0.0.1';
 
-app.get("/userId/:id/userName/:name", (req, res) => {
-    const id = req.params.id;
-    const name = req.params.name;
+app.get("/header", (req, res) => {
+    const id = req.header('id');
+    const name = req.header('name');
     res.send(`<h1>Hi I am  ${name} and My Roll No  ${id}</h1>`);
 });
 
--- requestServer/index.js
+++ requestServer/index.js
@@ -9,6 +9,16 @@ app.get("/", (req, res) => {
     const {id,name} = req.query;
     res.send(`Hi My name ${name} and my Id ${id}`);
 });
+app.get("/header", (req, res) => {
+    const id = req.header('id');
+    const name = req.header('name');
+    res.send(`<h1>Hi I am  ${name} and My Roll No  ${id}</h1>`);
+});
+app.get("/userId/:id/userName/:name", (req, res) => {
+    const id = req.params.id;
+    const name = req.params.name;
+    res.send(`<h1>Hi I am  ${name} and My Roll No  ${id}</h1>`);
+});
 
 app.listen(PORT,hostName,()=>{
     console.log(`server is running at http://${hostName}:${PORT}`);
