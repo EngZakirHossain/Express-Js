1b365849a17ea4dfabe5dd82c46057ba73d45bf5
Zakir Trimatric <zakirtrimatric@gmail.com>
2022-10-23 13:07:21 +0600

    post request with josn or from data

--- requestServer/index.js
+++ requestServer/index.js
@@ -1,7 +1,14 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const  app = express();
 const PORT = 3000;
 const hostName = '127.0.0.1';
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
     // const id = req.query.id;
@@ -19,6 +26,10 @@ app.get("/userId/:id/userName/:name", (req, res) => {
     const name = req.params.name;
     res.send(`<h1>Hi I am  ${name} and My Roll No  ${id}</h1>`);
 });
+app.post('/user',(req, res)=>{
+        const name = req.body.name;
+        res.send(`welcome ${name}`);
+})
 
 app.listen(PORT,hostName,()=>{
     console.log(`server is running at http://${hostName}:${PORT}`);
